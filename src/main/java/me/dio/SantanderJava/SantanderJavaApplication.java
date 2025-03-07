774a2e1bd741d9846a93a27c538c7f3e345adc6d
Vinicius Choma <109703798+viichoma@users.noreply.github.com>
2025-03-07 14:32:55 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderJava/SantanderJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.SantanderJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/SantanderJava/SantanderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.SantanderJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
