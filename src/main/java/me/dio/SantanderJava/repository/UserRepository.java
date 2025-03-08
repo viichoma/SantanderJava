7c14391d8f8080f90edae244612edac91be69a2a
Vinicius Choma <109703798+viichoma@users.noreply.github.com>
2025-03-08 17:09:15 -0300

    ,

---
+++ src/main/java/me/dio/SantanderJava/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.SantanderJava.repository;
+
+import me.dio.SantanderJava.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
